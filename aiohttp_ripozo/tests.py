c789740d3d19980212087fcbbb343f5a03e8cfcb
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-04-24 23:15:27 +0300

    Add docstrings to the tests module

--- aiohttp_ripozo/tests.py
+++ aiohttp_ripozo/tests.py
@@ -1,10 +1,14 @@
+"""Test suite for the adapter."""
 from unittest import mock, TestCase
 
 from .utils import get_request_mime_types_priorities
 
 
 class UtilsTest(TestCase):
+    """Test util functions."""
+
     def test_parse_mime_types(self):
+        """Ensure mime types are prioritized properly."""
         req = mock.MagicMock()
         header_val = ('Accept: application/vnd.siren+json, application/json, '
                       'text/json;q=0.9, */*;q=0.1, */*;q=1.0, text/html')
