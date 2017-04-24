cb0974afaa2088ff8d537b92ef8bf975ee009e4f
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-04-24 23:09:29 +0300

    Initialize basic testing

---
+++ aiohttp_ripozo/tests.py
@@ -0,0 +1,25 @@
+from unittest import mock, TestCase
+
+from .utils import get_request_mime_types_priorities
+
+
+class UtilsTest(TestCase):
+    def test_parse_mime_types(self):
+        req = mock.MagicMock()
+        header_val = ('Accept: application/vnd.siren+json, application/json, '
+                      'text/json;q=0.9, */*;q=0.1, */*;q=1.0, text/html')
+        with mock.patch.object(req.headers, 'getone', lambda hname: header_val):
+            prioritized_types = get_request_mime_types_priorities(req)
+
+        expected_res = (
+            (('Accept:',
+              'application/vnd.siren+json',
+              'application/json',
+              '*/*',
+              'text/html'),
+             1.0),
+            (('text/json',), 0.9),
+            (('*/*',), 0.1)
+        )
+
+        assert prioritized_types == expected_res
